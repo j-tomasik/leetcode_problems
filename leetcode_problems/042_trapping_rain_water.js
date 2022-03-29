64687b735f557d1c4936fbce30abc2e3ed21d832
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-03-29 12:47:57 -0700

    another day of grinding

--- leetcode_problems/042_trapping_rain_water.js
+++ leetcode_problems/042_trapping_rain_water.js
@@ -12,6 +12,27 @@ if (height === null) return 0;
         
         leftMax[i] = Math.max(leftMax[i-1], height[i])
     }
+
+    let lastIdx = height.length - 1;
+
+    let rightMax = new Array(lastIdx)
+    rightMax[lastIdx] = height[lastIdx]
+
+    for (let i = lastIdx - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(leftMax[i + 1], height[i])
+    }
+
+    let waterCount = 0;
+
+    for(let i = 1; i < lastIdx - 1; i ++) {
+        
+            let limit = Math.min(leftMax[i], rightMax[i]);
+            let water = limit - height[i];
+            waterCount += water;
+        
+    }
+
+    return waterCount;
     
     // do the same thing going from right to left for rightMax
     // iterate through one more time to calculate running total for water
