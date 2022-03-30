9d9919a77515c1cd0c8e1292c6e57774a0d06be9
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-03-29 18:18:43 -0700

    typo and new problem

--- structy problems/dynamic_programming/076_count_paths.js
+++ structy problems/dynamic_programming/076_count_paths.js
@@ -5,7 +5,7 @@ const countPaths = (grid, r=0, c=0, memo={}) => {
   if(r === grid.length || c === grid[0].length || grid[r][c] === 'X') return 0;
   if(r === grid.length - 1 && c === grid[0].length - 1) return 1;
   
-  const downCount = Paths(grid, r + 1, c, memo);
+  const downCount = countPaths(grid, r + 1, c, memo);
   const rightCount = countPaths(grid, r, c + 1, memo);
   
  memo[pos] = downCount + rightCount;
