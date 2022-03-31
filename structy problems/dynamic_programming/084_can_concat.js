4e338394ab5429e3cd425eec18e711248faa3096
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-03-31 16:19:36 -0700

    new js string compare method startsWith

---
+++ structy problems/dynamic_programming/084_can_concat.js
@@ -0,0 +1,20 @@
+const canConcat = (s, words,  memo={}) => {
+  if(s in memo) return memo[s];
+  if (s.length === 0) return true;
+ 
+  
+  for(let word of words) {
+    if(s.startsWith(word)) {
+      const suffix = s.slice(word.length)
+      if(canConcat(suffix, words, memo)) {
+        memo[s] = true;
+        return true;
+      } 
+    }
+  }
+  
+  
+  
+  memo[s] = false;
+  return false;
+};
