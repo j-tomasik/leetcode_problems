debf8b68e79759f2973157ca6ea2c0d13727f660
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-04-15 12:23:27 -0700

    really specific logic

---
+++ structy problems/mixed_recall/120_build_tree_in_post.js
@@ -0,0 +1,17 @@
+const buildTreeInPost = (inOrder, postOrder) => {
+  if(inOrder.length === 0) return null;
+  const value = postOrder[postOrder.length - 1];
+  const root = new Node(value);
+  const mid = inOrder.indexOf(value);
+  const leftInOrder = inOrder.slice(0, mid);
+  const rightInOrder = inOrder.slice(mid + 1);
+  const leftPostOrder = postOrder.slice(0, leftInOrder.length);
+  const rightPostOrder = postOrder.slice(leftPostOrder.length, -1)
+  
+  root.left = buildTreeInPost(leftInOrder, leftPostOrder);
+  root.right = buildTreeInPost(rightInOrder, rightPostOrder);
+  
+  
+  
+  return root;
+};
