ede9a427e4c7d30d1333a034e1a852d4aedff456
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-04-20 17:34:03 -0700

    will need some help understanding

---
+++ leetcode_problems/product_of_arr_except_self.js
@@ -0,0 +1,16 @@
+var productExceptSelf = function (nums) {
+  let products = new Array(nums.length).fill(1);
+  let currProduct = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    products[i] *= currProduct;
+    currProduct *= nums[i];
+  }
+
+  currProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    products[i] *= currProduct;
+    currProduct *= nums[i];
+  }
+  return products;
+};
