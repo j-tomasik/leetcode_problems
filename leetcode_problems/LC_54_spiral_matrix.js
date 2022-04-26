2b66590c1dac297c369ad1bcfe60163e3853de84
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-04-26 12:52:37 -0700

    pseudo coding

---
+++ leetcode_problems/LC_54_spiral_matrix.js
@@ -0,0 +1,6 @@
+//use 4 pointers
+//change direction of iteration when you hit a counter
+//counters are boundries
+//similar to an array flatten
+//push vals in one at a time as you iterate through
+//time complexity is O(row x col)
