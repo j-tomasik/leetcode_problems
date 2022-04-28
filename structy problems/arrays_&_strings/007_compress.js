c0a2a5376fe404f8823ee5e66a0d4ccfd2bd2fe6
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-04-28 11:49:06 -0700

    similar to last but opposite logic

---
+++ structy problems/arrays_&_strings/007_compress.js
@@ -0,0 +1,21 @@
+const compress = (s) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while(j <= s.length) {
+        if(s[i]=== s[j]) {
+            j += 1;
+        } else {
+            const num = j - i;
+            if(num === 1) {
+                result.push(s[i])
+            } else {
+                result.push(String(num))
+            }
+        }
+    }
+
+
+    return result;
+}
