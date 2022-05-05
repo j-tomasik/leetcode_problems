bf5a4309f34702901e1a01f9e8c13d7361e15f60
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-05 14:04:25 -0700

    muscle memory

--- structy problems/linked_lists/025_merge_lists.js
+++ structy problems/linked_lists/025_merge_lists.js
@@ -1,3 +1,30 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 const mergeLists = (head1, head2) => {
-    
+    let current1 = head1;
+    let current2 = head2;
+
+    let dummyNode = new Node(null);
+    let tail = dummyNode;
+
+    while(current1 !== null && current2 !== null) {
+        if(current1 < current2) {
+            tail.next = current1;
+            current1 = current1.next;
+        } else {
+            tail.next = current2;
+            current2  = current2.next;
+        }
+        tail = tail.next
+    }
+
+    if(current1 !== null) tail.next = current1;
+    if(current2 !== null) tail.next = current2;
+
+    return dummyNode.next;
 }
