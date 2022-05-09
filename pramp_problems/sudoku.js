f88beb5d27ea9857759da22dac96a2219ff8ffbe
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-09 11:55:47 -0700

    usre one set and string interpalate location info

--- pramp_problems/sudoku.js
+++ pramp_problems/sudoku.js
@@ -1,28 +1,29 @@
-function sudokuSolve(board) {
-  
-  
-  for(let i = 0; i < board.length; i++) {
-   let row = new Set();
-   let subBox = {};
-     
+const validSudoku = (board) => {
+  const set = new Set();
 
+  //i row, j columns
+  //check valid columns and rows O(9*9)
+  for(let i = 0; i < board.length; i++) {
     for(let j = 0; j < board[0].length; j++) {
-      let col = new Set();
-      
-      if(row.has(board[i][j])) return false;
-      let coord = i + "," + j;
-      subBox[coord] = board[i][j];
-      
-      
-      
-      let val = board[i][j];
-      
-      if(col.has(board[i][j])) return false;
-      row.add(val);
-      col.add(val);
+      const value = board[i][j];
+
+      if(value !== ".") {
+        const rowString = `${value} at row ${i}`;
+        const colString = `${value} at col ${j}`;
+        const boxString = `${value} at box ${Math.floor(i /3)}, ${Math.floor(j / 3)}`;
+
+        if(set.has(rowString) || set.has(colString) || set.has(boxString)) {
+          return false
+        } else {
+          set.add(rowString);
+          set.add(colString);
+          set.add(boxString);
+        }
+      }
     }
-    
+
   }
+  return true;
 }
 
 //the sudoku isnt solvable only if the exising numbers break one of the rules
