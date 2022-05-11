2944e1de5e763b8c8e629abb058d7bd584d66f41
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-11 13:46:41 -0700

    figured it out on my own

--- leetcode_problems/increasing_subs.js
+++ leetcode_problems/increasing_subs.js
@@ -1,19 +1,20 @@
 //iterate through
 //for each num, iterate up to k spots ahead,
-//if they nums keep increasing, at end incriment counter
-//if it doesnt get to k, move on to next
+//if they nums dont' keep increasing, break out and move to next start i point
+//if it does get to the end, iterate count
 const countIncreasingSubs = (arr, k) => {
     let count = 0;
 
     for(let i = 0; i < arr.length; i++) {
         for(let j = 1; j < k; j++) {
-            if(arr[i] < arr[i + j]) {
-                continue;
-            } else if(arr[i] < arr[i + j] && (j + 1) === k) {
-                    count += 1;
-            } else{
+            if(!(arr[i] < arr[i + j])) {
                 break;
+            } 
+
+            if(arr[i] < arr[i + j] && (j + 1) === k) {
+                count += 1;
             }
+        
         }
     }
     console.log(count);
@@ -21,4 +22,4 @@ const countIncreasingSubs = (arr, k) => {
 
 let test = [9,4,5,6,7,8,1];
 
-countIncreasingSubs(test);
+countIncreasingSubs(test, 3);
