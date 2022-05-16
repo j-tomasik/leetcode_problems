921a05cbfede44ddfb27676f55b030b1b7c1233b
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-16 16:34:20 -0700

    for loop recursive call

---
+++ structy problems/graphs/056_RECURSIVE_has_path.js
@@ -0,0 +1,12 @@
+const hasPath = (graph, src, dst) => {
+    if(src === dst) return true;
+
+    for(let neighbor of graph[src]) {
+        if(hasPath(graph, neighbor, dst) === true) {
+            return true;
+        }
+    }
+
+    return false;
+
+}
