109980b7597f8b740cbdf1c6b490196807b09120
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-18 10:11:46 -0700

    fixed while loop condition

--- leetcode_problems/two_sum_two_pointers.js
+++ leetcode_problems/two_sum_two_pointers.js
@@ -6,7 +6,7 @@ const twoSum = (arr, sum) => {
     let p1 = 0;
     let p2 = arr.length - 1;
 
-    while(arr[p1] + arr[p2] !== sum) {
+    while(p1 < p2) {
         if(arr[p1] + arr[p2] > target) {
             p2 -= 1;
         }
@@ -14,7 +14,11 @@ const twoSum = (arr, sum) => {
         if(arr[p1] + arr[p2] < sum) {
             p1 += 1;
         }
+
+        if(arr[p1] + arr[p2] !== sum) {
+            return [p1, p2];
+        }
     }
 
-    return [p1, p2];
+    return false;
 }
