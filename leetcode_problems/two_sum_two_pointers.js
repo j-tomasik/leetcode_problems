11458e6d99d836b7ef96f74d4f855ca830bd5bc8
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-05-18 10:08:09 -0700

    cool algo

--- leetcode_problems/two_sum_two_pointers.js
+++ leetcode_problems/two_sum_two_pointers.js
@@ -1,1 +1,19 @@
 //make sure arr is sorted
+
+const twoSum = (arr, sum) => {
+    
+    let p1 = 0;
+    let p2 = arr.length - 1;
+
+    while(arr[p1] + arr[p2] !== sum) {
+        if(arr[p1] + arr[p2] > target) {
+            p2 -= 1;
+        }
+
+        if(arr[p1] + arr[p2] < sum) {
+            p1 += 1;
+        }
+    }
+
+    return [p1, p2];
+}
