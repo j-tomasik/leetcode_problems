0d102eaa1ba7385a4612c0a9b78251d63bb4f93f
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-17 17:44:02 -0700

    base case and for loops

--- neetcode_google_qs/minimum_ticket_cost.js
+++ neetcode_google_qs/minimum_ticket_cost.js
@@ -1,4 +1,15 @@
-const minimumCost = (days, costs) => {
+const minimumCost = (days, costs, memo={}, i=0) => {
     //recursive dfs branching
     //memoize/cache to reduce duplicate logic
+    if(i in memo) return memo[i];
+    if (i === days.length) return 0;
+
+    memo[i] = Infinity;
+    let passes = [1, 7, 30];
+
+    for(let day of days) {
+        for(cost of costs) {
+
+        }
+    }
 }
