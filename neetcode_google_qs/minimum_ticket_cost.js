61621d2ecac9a3103ed2969a2141b3e2a7a732f2
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-17 17:53:45 -0700

    makes sense

--- neetcode_google_qs/minimum_ticket_cost.js
+++ neetcode_google_qs/minimum_ticket_cost.js
@@ -1,8 +1,19 @@
 const minimumCost = (days, costs) => {
     //recursive dfs branching
     //memoize/cache to reduce duplicate logic
-    const costs = [1, 7, 30];
+    const [one, seven, thirty] = costs;
     const memo = new Map();
 
-    
+    function recursive (curr, idx) {
+        if(memo.has(curr)) return memo.get(curr);
+        if(idx >= days.length) return 0;
+        if(curr >= days[idx]) return recursive(curr, idx + 1);
+        
+        const buy1 = recursive(days[idx], idx) + one;
+        const buy7 = recursive(day[idx]+6, idx) + seven;
+        const buy30 = recursive(days[idx]+29, idx) + thirty;
+
+        const min = Math.min(buy1, buy7, buy30);
+        memo.set(curr, min);
+    }
 }
