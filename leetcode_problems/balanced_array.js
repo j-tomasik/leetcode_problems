db36f5f69f8192d52efe080b7ce922dfde4a2689
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-20 13:44:05 -0700

    interesting while lopp

--- leetcode_problems/balanced_array.js
+++ leetcode_problems/balanced_array.js
@@ -4,9 +4,22 @@
 //incriment both pointers
 // once they cross, you have found index of pivor
 //while(leftIdx > rightIdx)
-//
+//smaller sum you incriment, larger sum you leave alone
 
 let leftSum = 0;
 let rightSum = 0;
 let leftIdx = 0;
 let rightIdx = arr.length - 1;
+
+while (leftIdx < rightIdx) {
+    if (leftSum < rightSum) {
+        leftSum += arr[leftIdx];
+        leftIdx += 1;
+    } else {
+        rightSum += arr[rightIdx];
+        rightIdx += 1;
+    }
+}
+
+return leftIdx;
+
