e61af8469defc6eac2ca8bb1348ad7fb69ec9085
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-20 13:40:01 -0700

    set up vars

---
+++ leetcode_problems/balanced_array.js
@@ -0,0 +1,12 @@
+//if left sum < right sum
+//
+
+//incriment both pointers
+// once they cross, you have found index of pivor
+//while(leftIdx > rightIdx)
+//
+
+let leftSum = 0;
+let rightSum = 0;
+let leftIdx = 0;
+let rightIdx = arr.length - 1;
