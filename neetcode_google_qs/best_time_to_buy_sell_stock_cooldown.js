3f589e5d2a44369e9d45a279dc558b72e040f9a0
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-06-23 18:38:09 -0700

    now you know

--- neetcode_google_qs/best_time_to_buy_sell_stock_cooldown.js
+++ neetcode_google_qs/best_time_to_buy_sell_stock_cooldown.js
@@ -1,2 +1,3 @@
 //this problem I would guess uses two pointers to traverse the array 
 //it will have to keep track of the largest distance between the two pointers
+//these two pointers will have to be continually comparing themselves to the prev max and min
