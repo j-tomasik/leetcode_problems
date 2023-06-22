59134d9396116d1245745f62f88523eb2a76f16f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-06-22 15:47:47 -0700

    pseudocode for merge k lists

---
+++ neetcode_google_qs/python_LL/mergeKLists.py
@@ -0,0 +1,12 @@
+#iterate through first of each ele to compare, hold pointer for each head's next node
+#keep looping through until all three are null, compare and add to main list
+
+#helper func to merge two sorted lists
+#main func:
+#edge case for not lists or len == 0
+#while lists has a len
+#for loop through lists arr incrementing by two
+#index in at i, and have logic to replace second with None if out of bounds
+#run merge lists on the two, append to a temp list that is empty and created in while loop(merged lists)
+#re-assign lists to be the merged lists temp arr
+#return lists an zero idx once while loop breaks
