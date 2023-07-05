2a9013940d1b05a23266edef373a3f6501ddc262
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-07-05 16:03:17 -0700

    bfs with deque for levels

---
+++ structy problems/binary_tree/102_tree_level_order_traversal.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def levelOrder(root):
+        list1 = []
+        q = deque()
+        q.append(root)
+        while q:
+            level = []
+            for i in range(len(q)):
+                poping = q.popleft()
+                if poping:
+                    level.append(poping.val)
+                    q.append(poping.left)
+                    q.append(poping.right)
+            if level:
+                list1.append(level)
+        return list1
+
