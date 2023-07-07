dce1b7014bf63be451f9e267df2c2ee5d150a6d1
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-07-07 10:47:52 -0700

    pseudo code

--- leetcode_problems/string_winner.py
+++ leetcode_problems/string_winner.py
@@ -1,1 +1,6 @@
-##
+##given s astring such as 'wwwbbbbwww' return the winner
+#w always goes first, you can only remove a char if it is surrounded by the same
+#if you cannot remove your char on your turn the other player wins
+
+#use a counter, plus on w and minus on a valid b
+#do logic to check who wins at the end
