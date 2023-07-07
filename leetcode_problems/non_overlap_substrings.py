a09e63093dad4dd8ddf740e8ba8bc451084357d6
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-07-07 11:37:37 -0700

    pseudo code

---
+++ leetcode_problems/non_overlap_substrings.py
@@ -0,0 +1,7 @@
+#given two strings, s and t, return the words in string s that are NOT in string t
+#example s = "I like cheese"
+#t = 'like' -> return ['I', 'like']
+
+# s = 'cats hats mats'
+# t = 'mats' -> return ['cats', 'hats']
+
