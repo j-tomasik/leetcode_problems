bb19c2cc0ab646c37698fbf4c855f01c987ad79a
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-07-07 11:15:23 -0700

    reeady to test

--- leetcode_problems/string_winner.py
+++ leetcode_problems/string_winner.py
@@ -4,3 +4,29 @@
 
 #use a counter, plus on w and minus on a valid b
 #do logic to check who wins at the end
+#-2 -> b wins
+#-1 -> b wins 'bbbwwwbbb'
+#0 -> b wins
+#1 -> w wins 'wwwbbbwww'
+#2 -> w wins 'wwwwbb'
+#3 -> w wins 'wwwwwwbb'
+
+def string_winnder(str):
+    count = 0
+    
+    for i, char in enumerate(str):
+        if char == 'w' and i > 0 and i < len(str) - 1:
+            if str[i-1] == 'w' and str[i+1] == 'w':
+                count += 1
+    
+    for i, char in enumerate(str):
+        if char == 'b' and i > 0 and i < len(str) - 1:
+            if str[i-1] == 'b' and str[i+1] == 'b':
+                count -= 1
+                
+    
+    if count <= 0:
+        return 'b'
+    else:
+        return 'w'
+    
