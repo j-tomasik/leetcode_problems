b9c3e0751720b60d3594e924721295db4ba985c0
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-08-17 20:47:55 -0700

    recursive memoizing

--- neetcode_google_qs/clone_graph.py
+++ neetcode_google_qs/clone_graph.py
@@ -2,3 +2,8 @@ def cloneGrapg(node):
     oldToNew = {}
     
     def dfs(node):
+        if node in oldToNew:
+            return oldToNew[node]
+        
+        copy = Node(node.val)
+        oldToNew[node] = copy
