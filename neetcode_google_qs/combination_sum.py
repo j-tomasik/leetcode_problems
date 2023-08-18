f619fef8d99dff2e1fef190180cba0f18f67d5a1
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-08-17 20:51:36 -0700

    dfs helper func

--- neetcode_google_qs/combination_sum.py
+++ neetcode_google_qs/combination_sum.py
@@ -1,2 +1,6 @@
-def combinationSum(candidates):
+def combinationSum(candidates, target):
+    res = []
     
+    def dfs(i, cur, sum):
+        if sum == target:
+            res.append(cur.copy())
