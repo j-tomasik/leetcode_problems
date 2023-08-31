77be0e47d9a731a42332b6b4c19051f74bc483a8
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-08-31 14:49:27 -0700

    test while loop inside for loop

--- neetcode_google_qs/dailyTemp.py
+++ neetcode_google_qs/dailyTemp.py
@@ -3,4 +3,8 @@ def dailyTemperatures(temperatures):
     stack = [] # pair:[temp, index]
     
     for i, t in enumerate(temperatures):
-        
+        while stack and t > stack[-1][0]:
+            stackTemp, stackIdx = stack.pop()
+            res[stackIdx] = i - stackIdx
+        stack.append((t, i))
+    return res
