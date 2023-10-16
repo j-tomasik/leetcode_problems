f2648c94506060ef13fadf8638fed1c520a4336d
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-16 14:07:22 -0700

    explaining code

--- neetcode_google_qs/lc_98_validate_binary_search_tree.py
+++ neetcode_google_qs/lc_98_validate_binary_search_tree.py
@@ -1,3 +1,6 @@
 ##DFS approach, every value right must be smaller than parents
 ##every child left must be greater than parent and it's parents
+##use two values to create a range for where a node must be between to be valid
+#O2(n) time complexity
+##use helper func that is recursive
 
