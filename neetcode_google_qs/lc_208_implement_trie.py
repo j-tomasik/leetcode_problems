374a9806153ec6ade4286608faa70e40112d185e
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-16 14:44:31 -0700

    explaining

--- neetcode_google_qs/lc_208_implement_trie.py
+++ neetcode_google_qs/lc_208_implement_trie.py
@@ -2,3 +2,6 @@
 #1-insert string 
 #2-search string, returns bool
 #3startsWith string, returns bool
+
+#graph of single chars, key in to instant lookup child letter
+#make sure to mark ends to confirm searched and saved word are complete
