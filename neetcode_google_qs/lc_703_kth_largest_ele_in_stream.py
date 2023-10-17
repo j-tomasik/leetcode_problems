f257e5148e7e4dc2a8502000d5de1d9c996a6d56
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-17 09:52:46 -0700

    init func

--- neetcode_google_qs/lc_703_kth_largest_ele_in_stream.py
+++ neetcode_google_qs/lc_703_kth_largest_ele_in_stream.py
@@ -1,3 +1,9 @@
 ##two hepler functions
 ##init and add 
 ##use a MIN HEAP the size of K
+
+class KthLargest:
+    
+    def __init__(self, k, nums):
+        self.minHeap, self.k = nums, k 
+        
