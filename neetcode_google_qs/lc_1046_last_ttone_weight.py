99532eb8c7a1132a5690168eb9623a7d42c5f134
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-17 10:48:45 -0700

    fun one

--- neetcode_google_qs/lc_1046_last_ttone_weight.py
+++ neetcode_google_qs/lc_1046_last_ttone_weight.py
@@ -2,3 +2,22 @@
 ##run while loop until len <= 1
 ##grab two biggest with idx -1 and -2
 ##compare and remove 
+
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones):
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+        
+        while len(stones) > 1:
+            first = heapq.heapop(stones)
+            second = heapq.heapop(stones)
+            
+            if second > first:
+                remainder = second - first
+                heapq.heappush(stones, remainder)
+                
+        stones.append(0)
+        return abs(stones[0])
