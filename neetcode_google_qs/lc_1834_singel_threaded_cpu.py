273a4dd783d45407857f71e6b09b788fcc3b3948
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-17 13:42:09 -0700

    pseudocode

---
+++ neetcode_google_qs/lc_1834_singel_threaded_cpu.py
@@ -0,0 +1,5 @@
+##min heap to use as queue to chose smallest time length task
+##keep track of time, add first num in duble to this
+##when adding to the min heap, add in original idx to add to return list
+##in the min heap rank the tasks by the time it takes to complete
+
