c5cb9c90e155849a3db1de306551ba48ee7fda6f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-17 09:55:11 -0700

    heapq.heappop

--- neetcode_google_qs/lc_703_kth_largest_ele_in_stream.py
+++ neetcode_google_qs/lc_703_kth_largest_ele_in_stream.py
@@ -2,8 +2,18 @@
 ##init and add 
 ##use a MIN HEAP the size of K
 
+import heapq
+
+
 class KthLargest:
     
     def __init__(self, k, nums):
         self.minHeap, self.k = nums, k 
+        heapq.heapify(self.minHeap)
+        
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+            
+    def add(self, val):
+        heapq.heappush(self.minHeap)
         
