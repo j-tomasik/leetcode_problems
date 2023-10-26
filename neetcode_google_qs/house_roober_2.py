f8e4f5dcb23255297272315762986d11e8008c12
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-26 16:49:39 -0700

    finished

--- neetcode_google_qs/house_roober_2.py
+++ neetcode_google_qs/house_roober_2.py
@@ -1,5 +1,5 @@
 def rob(nums):
-    return max(nums[0], helper(nums[1;]), helper(nums[:-1]))
+    return max(nums[0], helper(nums[1:]), helper(nums[:-1] )) 
 
 def helper(nums):
     rob1, rob2 = 0,0 
@@ -7,3 +7,5 @@ def helper(nums):
     for n in nums:
         newRob = max(rob1, rob2)
         rob1 = rob2
+        rob2 = newRob
+    return rob2
