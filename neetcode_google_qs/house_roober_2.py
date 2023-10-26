5f539af0e34b640c67e52ad7e4973bf964236b95
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-10-26 16:48:14 -0700

    helper func

--- neetcode_google_qs/house_roober_2.py
+++ neetcode_google_qs/house_roober_2.py
@@ -1,2 +1,9 @@
 def rob(nums):
     return max(nums[0], helper(nums[1;]), helper(nums[:-1]))
+
+def helper(nums):
+    rob1, rob2 = 0,0 
+    
+    for n in nums:
+        newRob = max(rob1, rob2)
+        rob1 = rob2
