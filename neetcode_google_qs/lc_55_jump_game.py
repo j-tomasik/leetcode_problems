9998b8354d89e582b3fa204e2252fc6724dd07eb
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-02 10:49:24 -0700

    too slow, need a greedy solution

--- neetcode_google_qs/lc_55_jump_game.py
+++ neetcode_google_qs/lc_55_jump_game.py
@@ -1,16 +1,30 @@
-class Solution(object):
-    def canJump(self, nums):
-        def recursive(nums, idx):
-            curr = nums[idx]
-            if curr == 0 and idx != len(nums) - 1:
-                return False
-            if idx == len(nums) - 1:
-                return True
 
-            for i in range(0, curr + 1):
-                newIdx = idx + i
-                poss = recursive(nums, newIdx)
-                if poss:
-                    return True
-            return False
-        return recursive(nums, 0)
+from ast import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]):
+        
+
+
+
+
+
+
+
+# class Solution(object):
+#     def canJump(self, nums):
+#         def recursive(nums, idx):
+#             curr = nums[idx]
+#             if curr == 0 and idx != len(nums) - 1:
+#                 return False
+#             if idx == len(nums) - 1:
+#                 return True
+
+#             for i in range(0, curr + 1):
+#                 newIdx = idx + i
+#                 poss = recursive(nums, newIdx)
+#                 if poss:
+#                     return True
+#             return False
+#         return recursive(nums, 0)
