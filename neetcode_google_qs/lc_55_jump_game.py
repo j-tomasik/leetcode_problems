5b16a709eb1297c0f9c1a494fc95900826fa24b4
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-02 10:33:25 -0700

    almost works goes too deep

---
+++ neetcode_google_qs/lc_55_jump_game.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def canJump(self, nums):
+        def recursive(nums, idx):
+            curr = nums[idx]
+            if curr == 0 and idx != len(nums) - 1:
+                return False
+            if idx == len(nums) - 1:
+                return True
+
+            for i in range(0, curr + 1):
+                newIdx = idx + i
+                poss = recursive(nums, newIdx)
+                if poss:
+                    return True
+            return False
+        return recursive(nums, 0)
