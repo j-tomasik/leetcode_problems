7460c13e583b8ac8b829873cb1ae571de913e558
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-03 13:17:41 -0700

    another greedy

--- lc_45_jump_game_2.py
+++ lc_45_jump_game_2.py
@@ -3,15 +3,15 @@ from ast import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        res = 0
+        res = 0#1
         l = r = 0
         
         while r < len(nums) - 1:
-            farthest = 0
-            for i in range(l, r + 1):
-                farthest = max(farthest, i + nums[i])
-            l = r + 1
-            r = farthest
+            farthest = 0#2
+            for i in range(l, r + 1):#1,2;
+                farthest = max(farthest, i + nums[i])#0vs2, 2vs1+3=4,4vs2+1=4
+            l = r + 1#1,2
+            r = farthest#2,4
             res += 1
             
         return res
