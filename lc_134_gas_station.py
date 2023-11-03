c50360ba15a48f444e344f655083339ed5335f9b
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-03 15:53:03 -0700

    Renaming vars

--- lc_134_gas_station.py
+++ lc_134_gas_station.py
@@ -3,12 +3,12 @@ class Solution:
         if sum(gas) < sum(cost):
             return -1
         total = 0
-        start = 0
+        res = 0
         for i in range(len(gas)):
             total += (gas[i] - cost[i])
             
             if total < 0:
                 total = 0
-                start = i + 1
+                res = i + 1
                 
-        return start
+        return res
