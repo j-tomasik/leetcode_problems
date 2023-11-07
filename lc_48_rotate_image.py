f268f393163442887f0922808e9b102b04f4d008
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-06 16:02:06 -0800

    pseudo code

--- lc_48_rotate_image.py
+++ lc_48_rotate_image.py
@@ -5,8 +5,16 @@ def rotate(matrix):
         for i in range(r - l):
             top, bottom = l, r
             
+            #save the topleft
             topLeft = matrix[top][l]
             
+            #move bottom left into top left
             matrix[top][l] = matrix[bottom][l]
             
+            #move bottom right into bottom left
             matrix[bottom][l] = matrix[bottom][r]
+            
+            #move top right into bottom right
+            matrix[bottom][r] = matrix[top][r]
+            
+            
