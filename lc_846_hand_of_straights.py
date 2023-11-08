c70faf0fe1445591b0e5e9d9025429f512dcf7a1
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-08 15:59:56 -0800

    use min heap to find starter val

--- lc_846_hand_of_straights.py
+++ lc_846_hand_of_straights.py
@@ -3,3 +3,8 @@ def isNStraightHand(hand, groupSize):
         return False
     
     count = {}
+    for n in hand:
+        count[n] = 1 + count.get(n, 0)
+        
+    minH = list(count.keys())
+    heapq.heapify(minH)
