2eff6a5a08791a7d70e9561bc7c3e48d429f4ea8
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-08 16:04:59 -0800

    Return true if false cases dont return early

--- lc_846_hand_of_straights.py
+++ lc_846_hand_of_straights.py
@@ -17,5 +17,8 @@ def isNStraightHand(hand, groupSize):
             if i not in count:
                 return False
             count[i] -= 1
-            if count[i] = 0:
-                if 
+            if count[i] == 0:
+                if i != minH[0]:
+                    return False
+                heapq.heappop(minH)
+    return True
