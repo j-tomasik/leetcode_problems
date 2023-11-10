67b533e26e37e381ad96c3109b3ab0b8845b4731
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-09 18:14:37 -0800

    checking input

--- coderByteProblems/codeland_username_validation.py
+++ coderByteProblems/codeland_username_validation.py
@@ -1,5 +1,12 @@
 def validInput(strParam):
+    if len(strParam) < 4 or len(strParam) > 25:
+        return False
     
+    if not strParam.startswith[0].isalpha():
+        return False
+    
+    if strParam[len(strParam)-1] == '_':
+        return False
     
     
     return True
