437f069feea8c808b51db00addd08f9ed70c688c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-13 15:07:56 -0800

    while loop inside for loop to increment

--- coderByteProblems/longest_word.py
+++ coderByteProblems/longest_word.py
@@ -4,6 +4,12 @@ def LongestWord(sen):
     words = sen.split(' ')
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     
-    
+    for word in words:
+        i = 0
+        currCount = 0
+        while i < len(word):
+            if word[i].lower() in alpha:
+                currCount += 1
+                i += 1
     
     return str(longest)
