ef755354a6e36944888c97f57653fbf29601782a
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-13 14:42:31 -0800

    psuedo

--- coderByteProblems/first_reverse.py
+++ coderByteProblems/first_reverse.py
@@ -1,5 +1,6 @@
 def first_reverse(strParam):
     #unpack a string of chars into a list with splat
     charsList = [*strParam]
-    
+    #reverse works like .sort() in python, does it in place
+    charsList.reverse()
     return strParam
