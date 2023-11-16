98ca5968e4fbe8bf4ae3d8a2a45dbf09c78aefd3
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-16 14:31:35 -0800

    check if more than 3 children

--- coderByteProblems/tree_constructor.py
+++ coderByteProblems/tree_constructor.py
@@ -3,6 +3,8 @@ def TreeConstructor(strArr):
     parentList = []
     for node in strArr:
         child, parent = eval(node)
-        
+        parentList.append(parent)
+        if parentList.count(parent) > 2:
+            return "false"
     
     return 'true'
