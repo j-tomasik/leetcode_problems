ab63479eaaedfc5d6788a3209ae00ccb22a51ffe
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-15 18:03:47 -0800

    creates sub counter for every iteration of i

--- coderByteProblems/min_window_substring.py
+++ coderByteProblems/min_window_substring.py
@@ -3,4 +3,10 @@ from collections import Counter
 EMPTY_COUNTER = Counter()
 
 def MinWindowSubstring(StrArr):
+    first, second = StrArr
     
+    frequencySecond = Counter(second)
+    options = []
+    
+    for i in range(len(first)):
+        curr = Counter()
