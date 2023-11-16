6f2edbd5671ef9ebd3be340bbae06679e35da72c
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-15 16:32:29 -0800

    exit early then check length at the end

--- coderByteProblems/bracket_matcher.py
+++ coderByteProblems/bracket_matcher.py
@@ -1,4 +1,12 @@
-def BracketMatcher(StrParam):
+def BracketMatcher(strParam):
     stack = []
     
+    for char in strParam:
+        if char == '(':
+            stack.append('(')
+        elif char == ')':
+            if len(stack) < 1:
+                return 0
+            else:
+                stack.pop()
     
