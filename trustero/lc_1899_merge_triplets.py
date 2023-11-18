ac256c40d27a6f8a15b7043bd9e263f3616fe264
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-17 16:38:40 -0800

    skip if any vals are too big

--- trustero/lc_1899_merge_triplets.py
+++ trustero/lc_1899_merge_triplets.py
@@ -2,4 +2,7 @@ def mergeTriplets(triplets, target):
     good = set()
     
     for t in triplets:
-        if t[0] > target[0]
+        if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+            continue
+        
+        for i, num in enumerate(t):
