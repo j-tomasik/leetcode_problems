7f8f70569b3e3c3568db0fcb79a2f7b504247d16
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-20 14:59:02 -0800

    for loop

--- lc_678_valid_parenthesis_string.py
+++ lc_678_valid_parenthesis_string.py
@@ -5,5 +5,8 @@ def checkValidString(s):
         if char == '(':
             lefMin, leftMx = lefMin + 1, leftMax + 1
         elif char == ')':
+            leftMin, leftMax = leftMin -1, leftMax - 1
+        else:
+            lefMin, leftMax = leftMin - 1, leftMax + 1
     
     return leftMin == 0
