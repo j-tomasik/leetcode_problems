fde31c87ecece1dd74c5ebcc5c2272f52ef0e6ee
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-20 14:16:23 -0800

    beautifully simple

--- lc_763_partition_labels.py
+++ lc_763_partition_labels.py
@@ -2,5 +2,16 @@ def partitionLabels(s):
     lastIndex = {}
     returnList = []
     for i, c in enumerate(s):
+        lastIndex[c] = i
+        
+    size, end = 0, 0
+    
+    for idx, char in enumerate(s):
+        size += 1
+        end = max(end, lastIndex[char])
+        
+        if idx == end:
+            returnList.append(size)
+            size = 0
     
     return returnList
