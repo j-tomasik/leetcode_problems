560eb3923e897ea919a0f043137b81b604d647b4
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-20 14:55:11 -0800

    big if block

--- lc_678_valid_parenthesis_string.py
+++ lc_678_valid_parenthesis_string.py
@@ -1,5 +1,9 @@
 def checkValidString(s):
     leftMin, leftMax = 0, 0
     
+    for char in s:
+        if char == '(':
+            lefMin, leftMx = lefMin + 1, leftMax + 1
+        elif char == ')':
     
     return leftMin == 0
