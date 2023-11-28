36a5469e0cd397bcabf8a446d88f1e8177d0555f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-28 13:59:52 -0800

    using python slice for faster copying

--- lc_46_permutations.py
+++ lc_46_permutations.py
@@ -1,5 +1,18 @@
 def permutations(nums):
     res = []
     
+    #base case
+    if(len(nums) == 1):
+        return [nums[:]]
     
+    for i in range(len(nums)):
+        n = nums.pop(0)
+        
+        perms = permutations(nums)
+        
+        for perm in perms:
+            perm.append(n)
+        res.extend(perms)
+        nums.apppend(n)
+        
     return res
