9e4dfb9a6c281fc36778d5845c928a8afcdd7c09
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-28 10:51:07 -0800

    always add, half the time pop

--- lc_78_subsets.py
+++ lc_78_subsets.py
@@ -4,5 +4,12 @@ def subsets(nums):
     cur = []
     def dfs(i):
         if i >= len(nums):
+            res.append(cur.copy())
             return
         
+        #decision to include nums[i]
+        cur.append(nums[i])
+        dfs(i + 1)
+        
+        #decision to NOT include nums[i]
+        cur.pop()
