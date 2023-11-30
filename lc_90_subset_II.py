d08e104e7c8f7c5dbf26c489c0af2835a6232c6f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-11-29 16:58:06 -0800

    Recursive basecase

--- lc_90_subset_II.py
+++ lc_90_subset_II.py
@@ -1,4 +1,10 @@
 def susetsWithDup(nums):
     res = []
+    nums.sort()
+    
+    def backtrack(i, subset):
+        if i == len(nums):
+            res.append(subset[::])
+            return
     
     return res
