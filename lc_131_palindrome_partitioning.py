97699c564c387c6f2da0168d5bdcd4a992d8cd47
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-05 11:42:42 -0800

    close but not exact

--- lc_131_palindrome_partitioning.py
+++ lc_131_palindrome_partitioning.py
@@ -1,5 +1,27 @@
 def palindrome_partitioning(s):
     res = []
+    def subset(s):
+        results = []
+        cur = []
+        def dfs(i):
+            if i >= len(s):
+                results.append(cur.copy())
+                return
+            cur.append(s[i])
+            dfs(i+1)
+            cur.pop()
+            dfs(i+1)
+        dfs(0)
+        return results
     
+    subs = subset(s)
+    # print(subs)
+    for sub in subs:
+        reversed = sub[::-1]
+        print(reversed)
+        if sub == reversed:
+            res.append(sub)
     
     return res
+
+palindrome_partitioning('aab')
