e3f84e77448d9a60cab9b08c3383569982c0fc16
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-06 11:45:53 -0800

    Recursive backtracking

--- lc_17_letter_combo_phone_number.py
+++ lc_17_letter_combo_phone_number.py
@@ -1,4 +1,21 @@
 def letterCombinations(digits):
     res = []
+    digits = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7'  : 'qprs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+    
+    def backtrack(i, curStr):
+        if len(curStr) == len(digits):
+            res.append(curStr)
+            return
+        
+        
     
     return res
