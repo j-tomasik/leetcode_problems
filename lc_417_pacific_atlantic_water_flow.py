e3995fb5582a0d73f2cc05bfa8508e85cb6207a5
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-07 16:18:40 -0800

    double for loop

--- lc_417_pacific_atlantic_water_flow.py
+++ lc_417_pacific_atlantic_water_flow.py
@@ -17,6 +17,18 @@ def pacificAtlantic(heights):
         dfs(r-1, c, visit, heights[r][c])
         dfs(r, c + 1, visit, heights[r][c])
         dfs(r, c - 1, visit, heights[r][c])
+    
+    for c in range(COLS):
+        dfs(0, c, pac, heights[0][c])
+        dfs(ROWS -1, c, atl, heights[ROWS -1][c])
+        
+    for r in range(ROWS):
+        dfs(r, 0, pac, heights[r][0])
+        dfs(r, COLS -1, atl, heights[r][COLS -1])
+        
     res = []
+    for r in range(ROWS):
+        for c in range(COLS):
+            
     
     return res
