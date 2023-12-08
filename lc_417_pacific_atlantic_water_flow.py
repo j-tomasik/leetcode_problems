47fbcd40e3814b8ab6cc65ce824f69d571de3334
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-07 16:15:10 -0800

    edge cases in helper func

--- lc_417_pacific_atlantic_water_flow.py
+++ lc_417_pacific_atlantic_water_flow.py
@@ -1,4 +1,18 @@
 def pacificAtlantic(heights):
+    ROWS, COLS = len(heights), len(heights[0])
+    pac, atl = set(), set()
+    
+    def dfs(r, c, visit, prevHeight):
+        if(
+            (r, c) in visit
+            or r < 0
+            or c < 0
+            or r == ROWS
+            or r == COLS
+            or heights[r][c] < prevHeight
+        ):
+            return
+        
     res = []
     
     return res
