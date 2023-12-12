09089a39c3b407c29c67ddf5b28289dabd8c63c1
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-11 16:39:49 -0800

    typewriter component

--- codeSandboxTakehome.js
+++ codeSandboxTakehome.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+const Typewriter = ({ text }) => {
+    const [displayedText, setDisplayedText] = useState('');
 
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex <= text.length) {
+        setDisplayedText(text.substring(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 500); // Adjust the delay here (in milliseconds)
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [text]);
+
+  return (
+    <div>
+      <h1>Typewriter Effect</h1>
+      <p>{displayedText}</p>
+    </div>
+  );
+};
 
 
 
