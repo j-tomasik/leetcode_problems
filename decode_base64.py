fb0390aa44c36462dc42921a4c4ac346214e753f
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-11 16:36:53 -0800

    new decode

--- decode_base64.py
+++ decode_base64.py
@@ -1,6 +1,6 @@
 import base64
 
-encoded_String = 'RnJhbmNpcw=='
+encoded_String = 'aHR0cHM6Ly90bnM0bHBnbXppaXlwbnh4emVsNXNzNW55dTBuZnRvbC5sYW1iZGEtdXJsLnVzLWVhc3QtMS5vbi5hd3MvcmFtcC1jaGFsbGVuZ2UtaW5zdHJ1Y3Rpb25zLw=='
 
 decoded_bytes = base64.b64decode(encoded_String)
 
