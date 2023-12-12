b6361b5a75e667f84ed3c1342987225508bc6396
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-11 16:51:22 -0800

    two compnents to combine

--- codeSandboxTakehome.js
+++ codeSandboxTakehome.js
@@ -27,6 +27,33 @@ const Typewriter = ({ text }) => {
   );
 };
 
+const URLFetcher = () => {
+  const [fetchedData, setFetchedData] = useState('');
+
+  useEffect(() => {
+    // Function to fetch the URL data
+    const fetchData = async () => {
+      try {
+        const response = await fetch('YOUR_URL_HERE'); // Replace 'YOUR_URL_HERE' with the fetched URL
+        const data = await response.text();
+        setFetchedData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setFetchedData('Error fetching data');
+      }
+    };
+
+    fetchData(); // Call the function to fetch data when the component mounts
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <div>
+      <ul></ul>
+      <p>{fetchedData}</p>
+    </div>
+  );
+};
+
 
 
 
