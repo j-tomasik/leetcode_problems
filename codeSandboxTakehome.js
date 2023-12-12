81b739dffdef3b7b63003759dec90a8da0ce2f2b
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-12 10:44:51 -0800

    finally working

--- codeSandboxTakehome.js
+++ codeSandboxTakehome.js
@@ -1,39 +1,50 @@
-import "./styles.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
 
 export default function App() {
-  const [fetchedData, setFetchedData] = useState('Loading...');
+  //sets page to display 'Loading...' on render and while fetch is ongoing
+  const [fetchedData, setFetchedData] = useState("Loading...");
+  //helper func, pass in response of the fetch call as a string 
+  //below inside the main use effect
+  const addListItems = (inputStr) => {
+    // sets state with list items
+    const newList = [];
 
-  const addListItemsWithDelay = (inputStr) => {
-    const newList = []
-    
-    for (let i = 0; i < inputStr.length; i++) {
+    //helper func for delay
+    const addWithDelay = (i) => {
       setTimeout(() => {
-        newList.push(<li key={i}>{inputStr[i]}</li>)
-        setFetchedData([...newList])
-      }, 1000 * 1);
+        newList.push(<li key={i}>{inputStr[i]}</li>);
+        setFetchedData([...newList]);
+        //half second delay for timewriter effect
+      }, 500 * i);
     };
+    //invokes delay helper func
+    for (let i = 0; i < inputStr.length; i++) {
+      addWithDelay(i);
+    }
   };
 
   useEffect(() => {
     // Function to fetch the URL data
     const fetchData = async () => {
       try {
-        const response = await fetch('https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/68616e'); 
+        const response = await fetch(
+          "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/68616e"
+        );
+        //converts response promise to a string
         const data = await response.text();
-        
-        addListItemsWithDelay(data)
+        //invokes helper func to modify the state
+        addListItems(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
-        setFetchedData('Error fetching data');
+        console.error("Error fetching data:", error);
+        setFetchedData("Error fetching data");
       }
     };
-
-    fetchData(); 
+    //invokes fetch
+    fetchData();
+    //runs only once on page render
   }, []);
 
-
- 
   return (
     <div className="App">
       <h1>Hello Ramp!</h1>
@@ -41,3 +52,29 @@ export default function App() {
     </div>
   );
 }
+  //SCRIPT USED FOR STEP 2
+//---------------------------------------------------------------------
+// function extractValuesFromSpecificTags() {
+//   const validCharacters = [];
+//   const codeElements = document.getElementsByTagName('code');
+
+//   for (let codeElement of codeElements) {
+//     const divElement = codeElement.querySelector('div');
+//     if (divElement) {
+//       const spanElement = divElement.querySelector('span');
+//       if (spanElement) {
+//         const charElement = spanElement.querySelector('i.char');
+//         if (charElement) {
+//           const character = charElement.getAttribute('value');
+//           validCharacters.push(character);
+//         }
+//       }
+//     }
+//   }
+
+//   const url = validCharacters.join('');
+//   return url;
+// }
+// const extractedURL = extractValuesFromSpecificTags();
+// console.log(extractedURL);
+//----------------------------------------------------------------------
