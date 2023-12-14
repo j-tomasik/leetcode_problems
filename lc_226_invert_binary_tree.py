5d1cbd149dfe8fd7631ed7822ba1a1ffac4f0633
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-14 13:35:42 -0800

    two recursive calls and double reassign

--- lc_226_invert_binary_tree.py
+++ lc_226_invert_binary_tree.py
@@ -1,4 +1,10 @@
 def invertTree(root):
+    if not root:
+        return None
     
-    
+    root.left, root.right = root.right, root.left
+
+    invertTree(root.left)
+    invertTree(root.right)
+        
     return root
