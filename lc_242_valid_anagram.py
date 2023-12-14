7f68e884a8f236e83926b84e7948e7060343e4ba
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-14 13:24:42 -0800

    fun one

--- lc_242_valid_anagram.py
+++ lc_242_valid_anagram.py
@@ -2,3 +2,12 @@ from collections import Counter
 def isAnagram(s, t):
     if len(s) != len(t):
         return False
+    
+    countS = Counter()
+    countT = Counter()
+    
+    for i in range(len(s)):
+        countS[s[i]] += 1
+        countT[t[i]] += 1
+        
+    return countS == countT
