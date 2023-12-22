40484003a6912aeb2f27077e6858ccd827795431
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-21 17:29:12 -0800

    binary search to split matrix in half

--- lc_74_search2d_matrix.py
+++ lc_74_search2d_matrix.py
@@ -2,6 +2,12 @@ def searchMatrix(matrix, target):
     ROWS, COLS = len(matrix), len(matrix[0])
     
     top, bot = 0, ROWS - 1
-    
+    while top <= bot:
+        row = (top + bot) // 2
+        if target > matrix[row][-1]:
+            top = row + 1
+        elif target < matrix[row][0]:
+            bot = row - 1
+        else: break
     
     return False
