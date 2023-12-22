ab3f5952d0bef3debc5699bab75b52ec1c8ae8aa
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-12-22 11:52:54 -0800

    while loop traverse

--- lc_138_copy_list_random_pointers.py
+++ lc_138_copy_list_random_pointers.py
@@ -1,2 +1,9 @@
 def copyRandomList(head):
     oldToCopy = {None: None}
+    
+    cur = head
+    while cur:
+        copy = Node(cur.val)
+        oldToCopy[cur] = copy
+    
+    return oldToCopy[head]
