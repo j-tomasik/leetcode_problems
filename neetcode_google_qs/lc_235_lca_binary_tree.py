1c0384446a7892999bc23feedc6cdaa788cb08d1
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-02 13:37:32 -0800

    quick and easy

--- neetcode_google_qs/lc_235_lca_binary_tree.py
+++ neetcode_google_qs/lc_235_lca_binary_tree.py
@@ -4,4 +4,7 @@ def lowestCommonAncestor(root, p, q):
     while temp:
         if p.val < temp.val and q.val < temp.val:
             temp = temp.left
-            
+        elif p.val > temp.val and q.val > temp.val:
+            temp = temp.right
+        else:
+            return temp
