11825169a0ff22e6d966dc3ee817526b9457d1ba
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-02 13:33:39 -0800

    Compare return vals of recurisve calls

--- neetcode_google_qs/lc_110_balanced-binary_tree.py
+++ neetcode_google_qs/lc_110_balanced-binary_tree.py
@@ -3,6 +3,9 @@ class Solution:
         return (self.Height(root) >= 0)
     
     def Height(self, root):
-        if root is None return 0
+        if root is None: return 0
         
-        leftHeight
+        leftHeight, rightHeight = self.Height(root.left), self.Height(root.right)
+        
+        if leftHeight < 0 or rightHeight < 0 or abs(leftHeight - rightHeight) > 1: return -1
+        
