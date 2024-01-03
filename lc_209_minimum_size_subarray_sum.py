5c51e89314a1e184bd35b8a9c6aefdccd405938c
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-03 14:11:38 -0800

    inf until replaced

---
+++ lc_209_minimum_size_subarray_sum.py
@@ -0,0 +1,9 @@
+def minSubArrayLen(target, nums):
+    minLength = float('inf')
+    currSum = 0
+    
+    
+    if minLength == float('inf'):
+        return 0
+    else:
+        return minLength
