a0d9820017cea9393e4529d3dc41db9b57e562d0
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-03 14:12:50 -0800

    r in range of len

--- lc_209_minimum_size_subarray_sum.py
+++ lc_209_minimum_size_subarray_sum.py
@@ -2,6 +2,14 @@ def minSubArrayLen(target, nums):
     minLength = float('inf')
     currSum = 0
     
+    l, r = 0,0
+    
+    for r in range(len(nums)):
+        currSum += nums[r]
+        
+        while currSum >= target:
+            
+    
     
     if minLength == float('inf'):
         return 0
