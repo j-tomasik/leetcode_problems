2655458c02524f1d53e023a11dabbe92a96a1509
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-04 15:24:13 -0800

    queue to bfs through, save level and node kv in map

--- lc_314_binary_tree_vertical-order_traversal.js
+++ lc_314_binary_tree_vertical-order_traversal.js
@@ -3,6 +3,21 @@ var verticalOrder = function(root) {
     const res = []
     const queue = [[root, 0]]
     const map = {}
-    
 
+    while(queue.length > 0) {
+        const [node, level] = queue.shift()
+
+        if(!map[level]) map[level] = [];
+        map[level].push(node.val)
+
+        if (node.left) {
+            queue.push([node.left, level-1])
+        }
+        if (node.right) {
+            queue.push([node.right, level + 1])
+        }
+    }
+    const levels = Object.entries(map).sort((a, b) => a[0]-b[0])
+    levels.forEach(x => res.push(x[1]))
+    return res;
 }
