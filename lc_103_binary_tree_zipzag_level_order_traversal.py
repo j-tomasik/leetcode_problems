7c8d9f7d3b2ae257db4f67541504779356de3c0b
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-05 14:36:19 -0800

    from collections import deque

--- lc_103_binary_tree_zipzag_level_order_traversal.py
+++ lc_103_binary_tree_zipzag_level_order_traversal.py
@@ -1,3 +1,9 @@
 ##bfs, use a queue
 ##every odd level needs to be reversed
 ##grab each row as a sub array then at the end iterate through sub arrays to reverse
+from collections import deque
+
+
+def zigzagLevelOrder(root):
+    res = []
+    q = deque([root] if root else [])
