44e05f08781b99a306f90bf7e158c95652640694
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-08 17:17:21 -0800

    nested if to check for good next to mid

--- lc_278_first_bad_version.py
+++ lc_278_first_bad_version.py
@@ -8,3 +8,12 @@ def firstBadVersion(n):
     lower = 1
     upper = n
     mid = math.ceil(n / 2)
+    
+    while lower < upper:
+        isBad = isBadVersion(mid)
+        if isBad:
+            if not isBadVersion(mid - 1):
+                return mid
+            else:
+                upper = mid
+                mid = math.ceil(upper / 2)
