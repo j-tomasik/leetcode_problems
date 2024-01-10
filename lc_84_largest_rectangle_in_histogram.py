fc0dc79997bacc657eb108a6a5ffdad0b8bde79b
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-09 18:17:26 -0800

    first while loop to remove larger vals than cur

--- lc_84_largest_rectangle_in_histogram.py
+++ lc_84_largest_rectangle_in_histogram.py
@@ -2,4 +2,12 @@ def largestReactangleArea(heights):
     maxArea = 0
     stack = []
     
+    for i, h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            index, height = stack.pop()
+            maxArea = max(maxArea, height * (i - index))
+            start = index
+        stack.append((start, h))
+    
     return maxArea
