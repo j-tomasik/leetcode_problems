adce313ceb6c9bbe02fafa0cecff09d0018e995d
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-16 17:20:08 -0800

    if else nested turnary

--- lc_56_merge_intervals.py
+++ lc_56_merge_intervals.py
@@ -5,5 +5,13 @@ def merge(intervals):
     currStart = intervals[0][0]
     currEnd = intervals[0][1]
     
+    for i in range(len(intervals)):
+        startInt, endInt = intervals[i]
+        
+        if startInt <= currEnd:
+            currEnd = endInt if endInt > currEnd else currEnd
+        else:
+            
+    
     
     return output
