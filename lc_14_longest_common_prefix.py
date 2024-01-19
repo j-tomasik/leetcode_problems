e789622e70b44d2fd471a2fb813169b7b289f6bb
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-19 12:28:34 -0800

    nested for loop

--- lc_14_longest_common_prefix.py
+++ lc_14_longest_common_prefix.py
@@ -1,4 +1,8 @@
 def longestCommonPrefix(strs):
     res = ''
+    for i in range(len(strs[0])):
+        for s in strs:
+            if s[i] != strs[0][i]:
+                return res
     
     return res
