1bee34702803522c5d80bf7cc2504666d729ec5a
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-23 17:11:42 -0800

    Dfs base case

--- lc_543_diameter_of_binary_tree.py
+++ lc_543_diameter_of_binary_tree.py
@@ -1,5 +1,10 @@
 def diameterOfBinaryTree(root):
     res = 0
     
+    def dfs(root):
+        #this allows us to mutate the outer level var instead of making a new one
+        nonlocal res
+        if not root:
+            return 0
     
     return res
