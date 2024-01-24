78fe1aacf3b7f924db7a1328f552c1ed914ee431
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-24 13:53:50 -0800

    makes more sense with comments

--- lc_98_validate_binary_search_tree.py
+++ lc_98_validate_binary_search_tree.py
@@ -4,3 +4,12 @@ def isValidBST(root):
         #base case, empty node is valid
         if not node:
             return True
+        #if not in right order, tree is invalid
+        if not (left < node.val < right):
+            return False
+        #LEFT CALL: curr is now left child of root, left edge is -infinity and largest is root 
+        #Right Call: curr is now right child of root, left is root, right is infinity 
+        
+        return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+    
+    return valid(root, float('-inf'), float('inf'))
