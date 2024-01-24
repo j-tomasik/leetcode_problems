8e4083e67aee0f3dd6dd22feb1921f0dc0af0d35
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-24 11:41:09 -0800

    another fun one

--- lc_104_maximum_depth_of_binary_tree.py
+++ lc_104_maximum_depth_of_binary_tree.py
@@ -3,3 +3,9 @@ def maxDepth(root):
     def dfs(root):
         if not root:
             return 0
+        
+        left = dfs(root.left)
+        right = dfs(root.right)
+        return 1 + max(left, right)
+    
+    return dfs(root)
