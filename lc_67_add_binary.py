c6952f7b3b6d1ad1f72a9f8393fb78155be1d785
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-23 16:54:18 -0800

    makes sense

--- lc_67_add_binary.py
+++ lc_67_add_binary.py
@@ -4,5 +4,22 @@ def affBinary(a, b):
     i = len(a) - 1
     j = len(b) - 1
     
+    #loop backwards, will reverse at the end, allows to append to list
+    while i >= 0 or j >= 0 or carry:
+        if i >= 0:
+            carry += int(a[i])
+            i -= 1
+        
+        if j >= 0:
+            carry += int(b[j])
+            j -= 1
+        
+        #this carry will either have 0, 1, 2 or 3. If both the prev vals are 1 in will add 1 to next digit
+        #if theres no carry it wont carry anything, if the carry is 1 then it will be 1
+
+        s.append(str(carry % 2))
+        #carry will be set to itself divided by 2, double slash rounds down to int,
+        #a single / would be a float for exact division 
+        carry //= 2
     
     return ''.join(reversed(s))
