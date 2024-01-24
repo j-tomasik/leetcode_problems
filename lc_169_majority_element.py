11c3bf219f343107eda4b4ae86c33999e2a069dd
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-23 16:34:20 -0800

    fun and easy :)

--- lc_169_majority_element.py
+++ lc_169_majority_element.py
@@ -2,4 +2,7 @@ from collections import Counter
 def majorityElement(nums):
     count = Counter()
     half = len(nums) / 2
-    
+    for num in nums:
+        count[num] += 1
+        if count[num] > half:
+            return num
