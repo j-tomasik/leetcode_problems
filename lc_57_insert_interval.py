6437968f423481cab4e54ffeb141bc70ba6fb7d2
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-29 14:52:17 -0800

    first case no merge

--- lc_57_insert_interval.py
+++ lc_57_insert_interval.py
@@ -1,4 +1,12 @@
-def insert(intervlas, newInterval):
+def insert(intervals, newInterval):
     res = []
+    
+    for i in range(len(intervals)):
+            #if new end is before curr start, no overlap
+            #add to returns, then return all seen and 
+            #slice everything after curr to concat and return
+        if newInterval[1] < intervals[i][0]:
+            res.append(newInterval)
+            return res + intervals[i:]
     
     return res
