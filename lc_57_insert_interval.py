5cd49e505c22fe1b6383e235c67ccfa18d9a4107
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-01-29 14:55:32 -0800

    its all about thinking like a programmer, systematically in divide up logic from pseudo code

--- lc_57_insert_interval.py
+++ lc_57_insert_interval.py
@@ -14,5 +14,16 @@ def insert(intervals, newInterval):
             #and keep iterating to find overlap
         elif newInterval[0] > intervals[i][1]:
             res.append(intervals[i])
-    
+            #there is an overlap of some type
+            #select min of starts for new start
+            #max of ends for new end
+            #don't append yet, just mutuate new and keep iterating
+        else:
+            newInterval = [
+                min(newInterval[0], intervals[i][0]),
+                max(newInterval[1], intervals[i][1]),
+            ]
+        #if still not added and overlapping end, do one 
+        #last add before returning
+    res.append(newInterval)
     return res
