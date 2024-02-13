f2db389085adfa589e7788e1d6e3edb778042dbb
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-02-12 16:02:38 -0800

    brute force solution

--- lc_73_set_matrix_zeros.py
+++ lc_73_set_matrix_zeros.py
@@ -6,5 +6,11 @@ def setZeroes(matrix):
         for c in range(len(matrix)):
             if matrix[r][c] == 0:
                 rowSet.add(r)
-    
+                colSet.add(c)
+
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if r in rowSet or c in colSet:
+                matrix[r][c] = 0
+        
     return matrix
