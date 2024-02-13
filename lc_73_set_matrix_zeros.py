5bd8a28184b0dd7424afbd8b73d33cb4223c78cf
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-02-12 16:01:31 -0800

    first double for loop

--- lc_73_set_matrix_zeros.py
+++ lc_73_set_matrix_zeros.py
@@ -2,5 +2,9 @@ def setZeroes(matrix):
     rowSet = set()
     colSet = set()
     
+    for r in range(len(matrix)):
+        for c in range(len(matrix)):
+            if matrix[r][c] == 0:
+                rowSet.add(r)
     
     return matrix
