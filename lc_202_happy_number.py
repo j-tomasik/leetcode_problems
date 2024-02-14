dc963e73e054a01e8c18321f3b18efc8c80e3ea9
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-02-13 16:47:41 -0800

    notes for main func

--- lc_202_happy_number.py
+++ lc_202_happy_number.py
@@ -1,11 +1,12 @@
 def isHappy(n):
     slow, fast = n, sumSquareDigits(n)
     
+        ##if slow and fast collide they are either both 1 or infinite loop
     while slow != fast:
         fast = sumSquareDigits(fast)
         fast = sumSquareDigits(fast)
         slow = sumSquareDigits(slow)
-    
+        ##once slow and fast are the same, either reduced and happy or infinite
     return True if fast == 1 else False
     
 def sumSquareDigits(n):
