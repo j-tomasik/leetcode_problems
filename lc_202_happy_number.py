ea58677678e6c81dcf9eb793a3e317fab0d2a111
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-02-13 16:46:40 -0800

    use helper on fast twice and slow once

--- lc_202_happy_number.py
+++ lc_202_happy_number.py
@@ -1,12 +1,19 @@
 def isHappy(n):
     slow, fast = n, sumSquareDigits(n)
     
-    def sumSquareDigits(n):
-        output = 0
+    while slow != fast:
+        fast = sumSquareDigits(fast)
+        fast = sumSquareDigits(fast)
+        slow = sumSquareDigits(slow)
+    
+    return True if fast == 1 else False
+    
+def sumSquareDigits(n):
+    output = 0
         
-        while n:
+    while n:
             ##modulo grabs last digit, then square it and add to output counter
-            output += (n % 10) ** 2
+        output += (n % 10) ** 2
             ##decrement n by dividing it by 10 to remove first digit and round down with double //
-            n = n // 10
-        return output
+        n = n // 10
+    return output
