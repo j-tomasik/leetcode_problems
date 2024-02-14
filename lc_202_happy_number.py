e85f797d688e9cbca334cfdf38857725551b61a5
Jack Tomasik <jackedwardtomasik@gmail.com>
2024-02-13 16:45:02 -0800

    helper func sums up each digit squared

--- lc_202_happy_number.py
+++ lc_202_happy_number.py
@@ -3,4 +3,10 @@ def isHappy(n):
     
     def sumSquareDigits(n):
         output = 0
+        
+        while n:
+            ##modulo grabs last digit, then square it and add to output counter
+            output += (n % 10) ** 2
+            ##decrement n by dividing it by 10 to remove first digit and round down with double //
+            n = n // 10
         return output
